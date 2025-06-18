8b8a67e935e0fd842c69a3a83ff47608884b6755
Sidney-Kenzo <50963836+Sidney-Kenzo@users.noreply.github.com>
2025-06-18 01:35:00 -0400

    Add files via upload

---
+++ TesteMain.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication4;
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author kenzo
+ */
+public class TesteMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         ConectaBanco banco = new ConectaBanco();
+
+        
+        String sqlInsert = "INSERT INTO test VALUES ('2', 'b')";
+        int linhasAfetadas = banco.executaSql(sqlInsert);
+        System.out.println("Linhas inseridas: " + linhasAfetadas);
+        
+        String sqlSelect = "SELECT * FROM test";
+        try {
+            ResultSet rs = banco.buscaDados(sqlSelect);
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("id") + 
+                                   ", Nome: " + rs.getString("texto"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // 4. Fecha a conexão
+        banco.finalizaConexao();
+    }
+    
+}
