7a8c483c1f9242a6bff609c5cac7ab8b08441353
Mateus Borges <mateusgaspio@gmail.com>
2025-06-20 18:43:03 -0400

    Estrutura reorganizada

--- PLANO_ALIMENTAR/src/ConexaoBD.java
+++ PLANO_ALIMENTAR/src/ConexaoBD.java
@@ -1,12 +1,10 @@
-package APP;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD {
     public static void main(String[] args) {
-        String url = "jdbc:postgresql://localhost:5433/TRABALHO_PRATICO";
+        String url = "jdbc:postgresql://localhost:5433/TESTE_TRABALHO_LBD";
         String usuario = "postgres";
         String senha = "minhasenha";
 
